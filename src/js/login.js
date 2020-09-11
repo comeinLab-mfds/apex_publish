e2d56a04054aca19bc863e52e952888a112af04f
coralcat <sori.coralcat@gmail.com>
2020-09-11 09:52:49 +0900

    dateRangeFilter customizing nrf 로고 삭제 및 위치 재정렬 APEX 폰트 변경, 문구 추가

--- src/js/login.js
+++ src/js/login.js
@@ -1,3 +1,15 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+})
+document.documentElement.addEventListener('touchstart', function (event) {
+    if (event.touches.length > 1) {
+        event.preventDefault()
+    }
+}, false)
+
 /*global $, document, window, setTimeout, navigator, console, location*/
 var usernameError = true,
         idError    = true,
